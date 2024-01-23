a7ee2a6ce16cb2413e079f96339ffb922dbbb0c7
ScarletWu <scarletruoxianwu@gmail.com>
2024-01-23 17:18:31 +0000

    improved termpaper

--- scripts/04-model.R
+++ scripts/04-model.R
@@ -20,6 +20,32 @@ library(broom)
 analysis_data <- read_csv("outputs/data/analysis_data.csv")
 analysis_data
 
+#### analyse real data ####
+
+descriptive_stats2 <- analysis_data %>%
+  summarise(
+    across(c(enrolled_population,
+             dtp_coverage_rate_percent,
+             dtp_religious_exemption_rate_percent, 
+             mmr_coverage_rate_percent,
+             mmr_religious_exemption_rate_percent),
+           list(mean = ~mean(., na.rm = TRUE), 
+                sd = ~sd(., na.rm = TRUE), 
+                min = ~min(., na.rm = TRUE), 
+                max = ~max(., na.rm = TRUE)))
+  )
+
+print(descriptive_stats2)
+
+# Correlation Analysis
+correlation_matrix2 <- cor(analysis_data %>% 
+                             select(enrolled_population,
+                                    dtp_coverage_rate_percent,
+                                    dtp_religious_exemption_rate_percent, 
+                                    mmr_coverage_rate_percent,
+                                    mmr_religious_exemption_rate_percent,
+                             ))
+print(correlation_matrix2)
 
 
 ### Model data ####
