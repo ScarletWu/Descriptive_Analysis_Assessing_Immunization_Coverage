b3f593291e49d08cad01c3e950d6c23f9dbcd7d5
ScarletWu <scarletruoxianwu@gmail.com>
2024-01-24 21:33:07 +0000

    updated the data cleaning part with data from 2017-2018

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -5,33 +5,58 @@
 # Contact: scarletruoxian.wu@utoronto.ca
 # Pre-requisites: 
 #install.packages("tidyverse")
+#install.packages("janitor")
 
 
 #### Workspace setup ####
+library(tidyverse)
+library(janitor)
 
 #### Basic cleaning ####
-raw_data <-
-  read_csv("/cloud/project/inputs/data/immunization.csv")
+raw_data1718 <-
+  read_csv("/cloud/project/inputs/data/immunization1718.csv")
+raw_data1819 <-
+  read_csv("/cloud/project/inputs/data/immunization1819.csv")
 
 # Make the names easier to type
-cleaned_data <-
-  clean_names(raw_data)
-cleaned_data
+cleaned_data1718 <-
+  clean_names(raw_data1718)
+cleaned_data1718
 
-selected_data <-
-  cleaned_data |>
+cleaned_data1819 <-
+  clean_names(raw_data1819)
+cleaned_data1819
+
+
+selected_data1718 <-
+  cleaned_data1718 |>
+  select(
+    school_name,
+    enrolled_population,
+    dtp_coverage_rate,
+    dtp_religious_exemption_rate,
+    mmr_coverage_rate,
+    mmr_religious_exemption_rate
+  )
+selected_data1718
+
+selected_data1819 <-
+  cleaned_data1819 |>
   select(
     school_name,
     enrolled_population,
     dtp_coverage_rate_percent,
     dtp_religious_exemption_rate_percent,
     mmr_coverage_rate_percent,
-    mmr_religious_exemption_rate_percent,
-   
+    mmr_religious_exemption_rate_percent
   )
+selected_data1819
 
-head(selected_data)
+
 
 #### Save data ####
-write_csv(cleaned_data, "outputs/data/cleaned_data.csv")
-write_csv(selected_data, "outputs/data/analysis_data.csv")
+write_csv(cleaned_data1718, "outputs/data/cleaned_data1718.csv")
+write_csv(selected_data1718, "outputs/data/analysis_data1718.csv")
+
+write_csv(cleaned_data1819, "outputs/data/cleaned_data1819.csv")
+write_csv(selected_data1819, "outputs/data/analysis_data1819.csv")
