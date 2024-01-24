6b22e8477093c85d71f9e832836d8ff9af8bf7c6
ScarletWu <scarletruoxianwu@gmail.com>
2024-01-24 21:14:41 +0000

    adding the data from 2017-2018 and redo the whole paper

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -14,7 +14,6 @@ library(tidyverse)
 library(dplyr)
 
 
-
 #### Download data ####
 
 # get package
@@ -24,14 +23,19 @@ package
 # get all resources for this package
 resources <- list_package_resources("99ff3657-b2e7-4005-a6fd-c36838ccc96d")
 
-# identify datastore resources; by default, Toronto Open Data sets datastore resource format to CSV for non-geospatial and GeoJSON for geospatial resources
+# identify datastore resources; by default, Toronto Open Data sets datastore 
+#resource format to CSV for non-geospatial and GeoJSON for geospatial resources
 datastore_resources <- filter(resources, tolower(format) %in% c('csv', 'geojson'))
 datastore_resources
-# load the first datastore resource for analysis
-data <- filter(datastore_resources, row_number()==1) %>% get_resource()
-data
 
+# load the first datastore resource for analysis
+data_1819 <- filter(datastore_resources, row_number()==1) %>% get_resource()
+data_1819
+
+data_1718 <- filter(datastore_resources, row_number()==4) %>% get_resource()
+data_1718
 
 #### Save data ####
 # I assigned the raw data as when you downloaded it.
-write_csv(data, "inputs/data/immunization.csv") 
+write_csv(data_1718, "inputs/data/immunization1718.csv") 
+write_csv(data_1819, "inputs/data/immunization1819.csv") 
